6aee40164a04b60b042f0802cb58d5559d7689f4
Johann-Sven-Lee-Uytanlet <114980320+Johann-Sven-Lee-Uytanlet@users.noreply.github.com>
2024-04-01 12:22:37 +0800

    trial edit still notworking

--- backend/index.js
+++ backend/index.js
@@ -8,25 +8,25 @@ app.use(express.json());
 
 const node1 = mysql.createConnection({
     host: '10.2.0.24',  // e.g., 'localhost'
-    user: 'STADVDB08_Server0',
+    user: 'root',
     password: 'ap4cAKqdRPfFv5xSg7MuW9mQ',
     database: 'mco2'
 });
-
+/*
 const node2 = mysql.createConnection({
     host: '10.2.0.25',  // e.g., 'localhost'
-    user: 'STADVDB08_Server1',
+    user: 'root',
     password: 'ap4cAKqdRPfFv5xSg7MuW9mQ',
     database: 'mco2'
 });
 
 const node3 = mysql.createConnection({
     host: '10.2.0.26',  // e.g., 'localhost'
-    user: 'STADVDB08_Server2',
+    user: 'root',
     password: 'ap4cAKqdRPfFv5xSg7MuW9mQ',
     database: 'mco2'
 });
-
+*/
 
 app.get("/", (req, res) => {
     res.json("hello");
--- trial.js
+++ trial.js
@@ -2,10 +2,11 @@ const mysql = require('mysql');
 
 // Create MySQL connection
 const connection = mysql.createConnection({
-    host: '10.2.0.24',  // e.g., 'localhost'
-    user: 'STADVDB08_Server0',
-    password: 'ap4cAKqdRPfFv5xSg7MuW9mQ',
-    database: 'mco2'
+    host: 'ccscloud.dlsu.edu.ph',  // e.g., 'localhost'
+    user: 'root',
+    password: 'root',
+    database: 'mco2',
+    port: '20024'
 });
 
 // Connect to MySQL
